ee967127fa188d1b4310e75a3ca7d444d7f5d97a
Matt Bank <mattbank8@gmail.com>
2017-07-11 13:35:07 -0400

    finished the JS project

---
+++ WebFundamentals/Javascript/declare.js
@@ -0,0 +1,19 @@
+var Num1 = 13;
+var Num2 = -99;
+var Num3 = 8;
+var Num4 = 624;
+var string1 = 'The Sweet Smell of Success';
+var string2 = "Easy Peasy Lemon Squeezy";
+var codingIsFun = true;
+var codingIsEasy = false;
+var ninja = undefined;
+
+console.log("The most unlucky number is", Num1)
+console.log("What number was Anit-Gretzky?", Num2)
+console.log("Matt Bank wore this number when he played lacrosse;", Num3)
+console.log("Think of a random number...", Num4)
+console.log("What was the Rock smelling?", string1)
+console.log("What is your favorite saying?", string2)
+console.log("True or false... learning to code is fun?", codingIsFun)
+console.log("True or false...learning to code is easy?", codingIsEasy)
+console.log("Give me something that doesn't work....", ninja)
