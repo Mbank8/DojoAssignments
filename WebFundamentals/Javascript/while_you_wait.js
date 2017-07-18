7e22bea73c562192816d37b4902048036c1b1ec4
Matt Bank <mattbank8@gmail.com>
2017-07-17 21:19:36 -0400

    created birthday countdown

---
+++ WebFundamentals/Javascript/while_you_wait.js
@@ -0,0 +1,25 @@
+var days = 60; 
+
+while (days >= 30){
+    console.log(days + " days until my birthday... Such a long time :(")
+
+    days -= 1;
+}
+while (days < 30 && days > 5){
+    console.log(days + " days until my birthday! we are almost there!")
+
+    days -= 1;
+}
+while (days <= 5 && days > 1){
+    console.log(days + " DAYS UNTIL MY BIRTHDAY! WOOOOOO!!!")
+
+    days -= 1;
+}
+
+while (days === 1){
+    console.log(days + " DAY UNTIL MY BIRTHDAY! WOOOOOOOO!!!!!")
+
+    days -= 1;
+}
+
+console.log("HAPPY BIRTHDAY PARTY TIMEEEEEE!!!");
