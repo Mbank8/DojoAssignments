fea649178166b7fcf0c9633d21691d7b02b192dd
Matt Bank <mattbank8@gmail.com>
2017-07-18 19:51:19 -0400

    fixed the code so that you can call function

--- WebFundamentals/Javascript/range_print.js
+++ WebFundamentals/Javascript/range_print.js
@@ -2,14 +2,24 @@
 //function needs a end (end is not included in the print
 //function needs a skip amount (count by ...)
 
-var start = 2 ;
-var end = 10 ;
-var skip = 2;
+// var start = 1 ;
+// var end = 3 ;
+// var skip = 1;
 
-function printRange(){
-    for(var i = start; i < end; i = i+skip){
-        console.log(i);
+// function printRange(){
+//     for(var i = start; i < end; i = i+skip){
+//         console.log(i);
+//     }
+
+// }
+// return printRange();
+
+
+
+function printRange(start, end, skip){
+    while(start < end){
+        console.log(start);
+        start += skip;
     }
-
 }
-return printRange();
+return printRange(2,10,2);
