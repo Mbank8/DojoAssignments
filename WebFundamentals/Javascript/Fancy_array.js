ef0c54174d6d04413555651b4ca1c32dbc87d0ab
Matt Bank <mattbank8@gmail.com>
2017-07-19 12:43:36 -0400

    Switched the order of arr[0] and arr[3]

--- WebFundamentals/Javascript/Fancy_array.js
+++ WebFundamentals/Javascript/Fancy_array.js
@@ -3,6 +3,9 @@
 
 function fancyArray(arr){
 var arr = ["James", "Jill", "Jane", "Jack"];
+temp = arr[0];
+arr[0] = arr[3];
+arr[3] = temp;
     console.log[arr]
     for(var i = 0; i < arr.length; i++) {
         console.log(i + " -> " + arr[i]);
