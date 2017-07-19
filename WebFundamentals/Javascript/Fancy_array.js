7f02b9074ad5ffa24af3429a8433a748b4762d38
Matt Bank <mattbank8@gmail.com>
2017-07-19 12:39:48 -0400

    Created basic fancy array function

---
+++ WebFundamentals/Javascript/Fancy_array.js
@@ -0,0 +1,12 @@
+//write a function that returns arr # -> Name
+//arr = ["James", "Jill", "Jane", "Jack"]
+
+function fancyArray(arr){
+var arr = ["James", "Jill", "Jane", "Jack"];
+    console.log[arr]
+    for(var i = 0; i < arr.length; i++) {
+        console.log(i + " -> " + arr[i]);
+        
+    }
+}
+return fancyArray()
