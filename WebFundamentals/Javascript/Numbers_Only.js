79b1d31896bfde08358707e926277cebc45d33a4
Matt Bank <mattbank8@gmail.com>
2017-07-19 20:32:26 -0400

    Array only shows numbers

---
+++ WebFundamentals/Javascript/Numbers_Only.js
@@ -0,0 +1,17 @@
+//create a function that returns an array with only the numbers 
+//var newArray = numbersOnly
+//array = [1, "apple", -3, "orange", 0.5]
+
+
+function numbersOnly(){
+    var arr = [1, "apple", -3, "orange", 0.5]
+    var newArray = [];
+    
+    for (var i = 0; i < arr.length; i++){
+        if(typeof arr[i] === "number"){
+            newArray.push(arr[i]);
+        }
+        console.log(newArray);
+    }
+}
+numbersOnly()
