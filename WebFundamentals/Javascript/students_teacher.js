b9e44bdb3d368cf95c41c8d8846e6930690a94f6
Matt Bank <mattbank8@gmail.com>
2017-07-19 21:33:57 -0400

    finished part one of this project

---
+++ WebFundamentals/Javascript/students_teacher.js
@@ -0,0 +1,24 @@
+/* given the following array
+var students = [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+
+     Create a program that outputs:
+
+Michael Jordan
+John Rosales
+Mark Guillen
+KB Tonel
+]*/
+
+var students = [ 
+     {first_name:  'Michael', last_name : 'Jordan'},
+     {first_name : 'John', last_name : 'Rosales'},
+     {first_name : 'Mark', last_name : 'Guillen'},
+     {first_name : 'KB', last_name : 'Tonel'}
+];
+for (var fullname in students) {
+    console.log(students[fullname].first_name, students[fullname].last_name);
+}
