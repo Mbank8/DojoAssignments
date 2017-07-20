c16f1d253e166053daba8a47ce364310cecb80e5
Matt Bank <mattbank8@gmail.com>
2017-07-19 21:48:22 -0400

    partialy done part 2. still stuck on a few things

--- WebFundamentals/Javascript/students_teacher.js
+++ WebFundamentals/Javascript/students_teacher.js
@@ -13,12 +13,32 @@ Mark Guillen
 KB Tonel
 ]*/
 
-var students = [ 
+// var students = [ 
+//      {first_name:  'Michael', last_name : 'Jordan'},
+//      {first_name : 'John', last_name : 'Rosales'},
+//      {first_name : 'Mark', last_name : 'Guillen'},
+//      {first_name : 'KB', last_name : 'Tonel'}
+// ];
+// for (var fullname in students) {
+//     console.log(students[fullname].first_name, students[fullname].last_name);
+// }
+
+
+var users = {
+ 'Students': [ 
      {first_name:  'Michael', last_name : 'Jordan'},
      {first_name : 'John', last_name : 'Rosales'},
      {first_name : 'Mark', last_name : 'Guillen'},
      {first_name : 'KB', last_name : 'Tonel'}
-];
-for (var fullname in students) {
-    console.log(students[fullname].first_name, students[fullname].last_name);
+  ],
+ 'Instructors': [
+     {first_name : 'Michael', last_name : 'Choi'},
+     {first_name : 'Martin', last_name : 'Puryear'}
+  ]
+ }
+
+var num = 1; 
+for (var student_name in "Students"){
+    console.log(num + " - " + "Students"[student_name].first_name, "Students"[student_name].last_name + " - " + student_name.length);
+    num++;
 }
