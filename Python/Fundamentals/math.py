5faede99ac715650d602f50cebb4116bd8a60ac0
Matt Bank <mattbank8@gmail.com>
2017-08-01 09:39:34 -0400

    finished math algo's

---
+++ Python/Fundamentals/math.py
@@ -0,0 +1,20 @@
+#prints all odd integers from 1 to 1000
+# for odd in range(1,1000):
+#     if odd % 2 != 0:
+#         print odd
+#         odd += 1
+
+#prints all multiples of 5 from 5 to 1,000,000
+# for five in range(5,1000000):
+#     if five % 5 == 0:
+#         print five
+#         five += 1
+
+#prints the sum of all the values in the list
+# a = [1, 2, 5, 10, 255, 3]
+# b = sum(a)
+# print b
+
+#prints the average of the values in the list
+# a = [1, 2, 5, 10, 255, 3]
+# print sum(a)/len(a)
