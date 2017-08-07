a1824004c2468d929b13d9438e76763d15978ef7
Matt Bank <mattbank8@gmail.com>
2017-08-07 11:18:56 -0400

    basic name recall dictionary

---
+++ Python/Fundamentals/dict_basics.py
@@ -0,0 +1,7 @@
+myInfo = {"name":"Anna", "age":"101", "county": "The United States", "language":"Python"}
+
+for  data in myInfo:
+    print "My name is", myInfo["name"]
+    print "My age is", myInfo["age"]
+    print "My country of birth is", myInfo["county"]
+    print "My favorite language is",myInfo["language"]
