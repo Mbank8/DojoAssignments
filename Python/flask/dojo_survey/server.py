5ff127fe10bc1fcff0750c43a57514a7468dbf01
Matt Bank <mattbank8@gmail.com>
2017-08-14 16:04:44 -0400

    made validation changes to the survey

--- Python/flask/dojo_survey/server.py
+++ Python/flask/dojo_survey/server.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request, redirect
+from flask import Flask, render_template, request, redirect, session, flash
 app = Flask(__name__)
+app.secret_key = "Thisisabigsecret"
 
 @app.route('/')
 def index():
@@ -7,9 +8,27 @@ def index():
 
 @app.route('/result', methods=['post'])
 def results():
-    name=request.form['name']
-    location=request.form['location']
-    language=request.form['language']
-    comment=request.form['comment']
-    return render_template('/result.html', name = name, location = location, language = language, comment = comment)
+    errors = False
+
+    if len(request.form['name']) < 1:
+        flash("Please enter valid name")
+        errors = True
+
+    if len(request.form['location']) < 1:
+        flash("Please enter valid location")
+        errors = True
+
+    if len(request.form['language']) < 1:
+        flash("Please enter valid language")
+        errors = True
+
+    if len(request.form['comment']) > 120:
+        flash("Please keep comments between 1 and 120 characters")
+        errors = True
+    
+    if errors == True:
+        return redirect('/')
+    else:
+        return render_template('result.html', user = request.form)
+    
 app.run(debug=True)
